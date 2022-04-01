8fcc3bcb5ab63ea5f127c06d4d45c9d257d122cd
Perfomo <31494.perfomo@gmail.com>
2022-04-01 22:51:29 +0300

    Add new rec GetLevel func, last GetLevel func deleted

--- Lab5/lab5.cpp
+++ Lab5/lab5.cpp
@@ -24,227 +24,53 @@ struct Tree
     Tree *left, *right;
 };
 
-Tree* WhatPref(int level_now, Tree* root, int& new_lev)
+int NextLevel(Tree* root, int level_now)
 {
-    int amount = level_now;
-    Tree* t = root, *trash;
-    while(level_now>0)
+    Tree* t = root, *stop;
+    int left = 0, right = 0;
+    level_now++;
+    if(t -> left)
     {
-        for (int i = 0; i < level_now; i++)
-        {
-            if (t -> left)
-            {
-                t = t -> left;
-            }
-            else
-            {
-                if ( t -> right and i != level_now-1)
-                {
-                    t = t -> right;
-                }
-                else
-                {
-                    if(trash -> right != t)
-                    {
-                        t =  trash -> right;
-                        new_lev = level_now - 1;
-                        return t;
-                    }
-                    else
-                    {
-                        i = level_now;
-                    }
-                }
-            }
-            trash = t;  
-        }
-        if ( level_now == 0)
-        {
-            new_lev = 0;
-            return root;
-        }
-        level_now--;
+        t = t -> left;
+        left = NextLevel(t, level_now);
+        t = root;
     }
-    new_lev = 0;
-    return root;
-}
-
-
-int WhatLevel(Tree* root)
-{
-    Tree* t = root, *stop, *trash, *new_t;
-    int level = 0, level_now = 0, kol = 0;
-    while(kol < 2)
-    {
-        if (t -> left)
-        {
-            t = t -> left;
-            ++level_now;
-        }
-        else
-        {
-            if ( t -> right)
-            {
-                t = t -> right;
-                ++level_now;
-            }
-            else
-            {
-                stop = trash;
-                level_now--;
-                if(stop -> right and stop -> right != t)
-                {
-                    t = stop -> right;
-                }
-                else
-                {
-                    t = WhatPref(level_now, root, level_now);
-                    if (t == root)
-                    {
-                        t = root -> right;
-                        level_now++;
-                        kol++;
-                    }
-                }
-            }
-        }
-        trash = t;
-        if ( level_now > level)
-        {
-            level = level_now;
-        }
-    }
-    return level;
-}
-
-
-Tree* LastStop(Tree* root)
-{
-    Tree* t = root;
-    while(t)
-    {
-        if (t -> left)
-        {
-            t = t -> left;
-        }
-    }
-}
-
-int RecSearch(int level_now, int level, Tree* root)
-{
-    Tree* t = root;
-
-    for(int i = 0; i <= level_now; i++)
-    {
-        if(t -> left)
-        {
-            t = t -> left;
-        }
-        else
-        {
-            if(t -> right)
-            {
-                t = t -> right;
-            }
-            if(level_now == level)
-            {
-                return t -> key;
-            }
-        }
-    }
-    if(t->right)
+    if(t -> right)
     {
         t = t -> right;
-        return RecSearch(level_now+1, level, root);
+        right = NextLevel(t, level_now);
+    }
+    if (left == 0 and right == 0)
+    {
+        return --level_now;
+    }
+    if(left > right)
+    {
+        return left;
+    }
+    return right;
+}
+
+int GetLevel(Tree* root)
+{
+    int left = 0, right = 0;
+    if (root -> left)
+    {
+        left = NextLevel(root -> left, 1);
+    }
+    if (root -> right)
+    {
+        right = NextLevel(root -> right, 1);
+    }
+    if (left > right)
+    {
+        return left;
     }
     else
     {
-        return RecSearch(level_now-1, level, root);
-    }
-     
-}
-
-int* FindOnLevel(Tree* root, int level)
-{
-    int ready = 0, level_of_view = 0, amount = pow(2,level), stop = 0;
-    Tree* t = root;
-    bool add = true;
-    int* level_nums = new int(amount);
-    // while (ready != amount)
-    // {
-        // for(int i = 0; i < level; i++)
-        // {
-        //     if(t -> left)
-        //     {
-        //         t = t -> left;
-        //     }
-        //     else
-        //     {
-        //         if(t -> right)
-        //         {
-        //             t = t -> right;
-        //         }
-        //         else
-        //         {
-        //             if(i == level - 1)
-        //             {
-        //                 level_nums[ready] = t -> key;
-        //                 ready++;
-        //             }
-        //             else
-        //             {
-        //                 for (int i = 0; i <= amount; i++)
-        //                 level_nums[i] = RecSearch(i, level, root);
-        //             }
-        //         }
-        //     }
-        // }      
-    // }
-    // for (int i = 0; i <= amount; i++)
-    // {
-    //     level_nums[i] = RecSearch(4, level, root);
-
-    // }
-    // return level_nums;
-}
-
-void view(Tree *root, int level)
-{
-    Tree *t = root, *left = root -> left, *right = root -> right;
-    int line_size = 31, i = 0;
-    int* mas = FindOnLevel(root, 4);
-    while(i <= level)
-    {
-        cout<< mas[i] << endl;
+        return right;
     }
 }
-
-string rec_view(Tree *root)
-{
-    int line = 31;
-    Tree* left = root -> left, *right = root -> right;
-    string str1 = "", str2 = "";
-    if(left)
-    {
-        str1 = rec_view(left);
-    }
-    if (right)
-    {
-        str2 = rec_view(right);
-    }
-    cout << str1 + str2 << endl;
-    return "";
-}
-
-// void ur_view(Tree *root)
-// {
-//     string level;
-//     int level;
-//     Tree* left, *riht;
-
-// }
-
-
 
 Tree *CreateTree()
 {
@@ -332,7 +158,8 @@ int main()
             AddValues(root, n);
             break;
         case '3':
-            cout << WhatLevel(root) << endl;
+            // cout << WhatLevel(root) << endl;
+            cout << lev(root) << endl;
         default:
             break;
         }
